639d5ccd09f763bfe32a621d73d05844b84c7fb1
Mert OZan <119129824+OziMoa@users.noreply.github.com>
2023-02-14 10:40:56 +0300

    Create MaximumSubarraySum.py

---
+++ kyu5/MaximumSubarraySum.py
@@ -0,0 +1,29 @@
+"""DESCRIPTION:
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+"""
+
+def max_sequence(arr):
+    if len(arr)==0:
+        return 0
+    max = 0 
+    
+    local_max = 0
+    
+    for i in arr: 
+        if local_max > 0: 
+            local_max +=i
+            if local_max < 0:
+                local_max = 0
+            elif local_max > max :
+                 max = local_max
+        elif i > 0: 
+            local_max += i
+    
+    return max 
+    pass
